349e03a38a53c5626c7c5fd3e27cf52de6a666ca
Harsh Tyagi <tyagiharsh216@gmail.com>
2023-08-30 17:19:21 +0530

    client code

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './componenet/Navbar';
+import Carousal from './componenet/Carousal';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    <Navbar/>
+    <Carousal/>
+    </div>
+  );
+}
+
+export default App;
